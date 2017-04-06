93ae5d9850da896217b2d5fe259ddf8437f332fc
root <1501671995@qq.com>
2017-04-06 13:40:53 +0800

    第二次提交—新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/example/horizon/coolweather/model/Province.java
@@ -0,0 +1,35 @@
+package com.example.horizon.coolweather.model;
+
+/**
+ * Created by horizon on 4/5/2017.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
